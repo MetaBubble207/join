4309051eaf439a93580e803c890af4d84a1225cd
linjingcheng <1152691418@qq.com>
2022-10-05 17:08:58 +0800

    init

--- src/views/login-form-10/index.jsx
+++ src/views/login-form-10/index.jsx
@@ -2,7 +2,7 @@
  * @Author: linjingcheng 1152691418@qq.com
  * @Date: 2022-09-27 19:30:06
  * @LastEditors: linjingcheng 1152691418@qq.com
- * @LastEditTime: 2022-10-02 20:12:19
+ * @LastEditTime: 2022-10-03 10:20:33
  * @FilePath: \metabubble-join\src\views\login-form-10\index.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,10 +17,10 @@ import { useNavigate } from 'react-router-dom'
 
 const LoginForm = () => {
     const [vantaEffect, setVantaEffect] = useState(null)
-    const [email,setEmail] = useState(undefined)
-    const [password,setPassword] = useState(undefined)
+    const [email, setEmail] = useState(undefined)
+    const [password, setPassword] = useState(undefined)
     const history = useNavigate()
-    const [way,setWay]  = useState('login')
+    const [way, setWay] = useState('login')
     const myRef = useRef(null)
     useEffect(() => {
         if (!vantaEffect) {
@@ -44,44 +44,44 @@ const LoginForm = () => {
     const signup = async () => {
         try {
             const res = await api.signup({
-                schoolId :email,
-                password:password
+                schoolId: email,
+                password: password
             });
             console.log(res)
-            if(res){
+            if (res) {
                 Notify.show({ type: 'success', message: '注册成功' })
-                window.localStorage.setItem("token",res.access_token);
-                service.defaults.headers.common = {'Authorization': `bearer ${res.access_token}`}
+                window.localStorage.setItem("token", res.access_token);
+                service.defaults.headers.common = { 'Authorization': `bearer ${res.access_token}` }
                 history('/profile')
             }
-            
+
         } catch (error) {
-            console(error,'singup err')
+            console(error, 'singup err')
             Notify.show('注册失败，请联系管理员')
         }
     }
-    const signin = async () =>{
+    const signin = async () => {
         try {
             const res = await api.signin({
-                schoolId :email,
-                password:password
+                schoolId: email,
+                password: password
             });
             console.log(res)
-            if(res){
-                service.defaults.headers.common = {'Authorization': `bearer ${res.access_token}`}
-                window.localStorage.setItem("token",res.access_token);
+            if (res) {
+                service.defaults.headers.common = { 'Authorization': `bearer ${res.access_token}` }
+                window.localStorage.setItem("token", res.access_token);
                 history('/profile')
             }
         } catch (error) {
-            console(error,'login err')
+            console(error, 'login err')
             Notify.show('登录失败，请联系管理员')
         }
     }
-    const submit = ()=>{
-        if(way === 'login'){
-            signin()
-        }else{
-            signup()
+    const submit = async () => {
+        if (way === 'login') {
+            await signin()
+        } else {
+            await signup()
         }
     }
     return (
@@ -108,28 +108,28 @@ const LoginForm = () => {
                                         </p>
                                     </div> */}
                                 </div>
-                                <form action="#" className="signin-form">
+                                <form className="signin-form">
                                     <div className="form-group mt-3">
-                                        <input type="text" className="form-control" required value={email} onChange={(e)=>{
+                                        <input type="text" className="form-control" value={email} onChange={(e) => {
                                             setEmail(e.target.value)
                                         }} />
                                         <label className="form-control-placeholder" htmlFor="username">学号</label>
                                     </div>
                                     <div className="form-group">
                                         <input id="password-field" type="password" className="form-control"
-                                         autoComplete="on"
-                                        required
-                                        value={password} onChange={(e)=>{
-                                            setPassword(e.target.value)}} />
+                                            autoComplete="on"
+                                            value={password} onChange={(e) => {
+                                                setPassword(e.target.value)
+                                            }} />
                                         <label className="form-control-placeholder " toggle="#password-field" htmlFor="password">密码</label>
                                         {/* <span toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span> */}
                                     </div>
                                     <div className="form-group">
-                                        <button onClick={submit} type="submit" className="form-control btn btn-primary rounded submit px-3" >{way==='login'?"登录":"注册"}</button>
+                                        <button onClick={submit} type="button" className="form-control btn btn-primary rounded submit px-3" >{way === 'login' ? "登录" : "注册"}</button>
                                     </div>
 
                                 </form>
-                                <p className="text-center"> <a data-toggle="tab" href="#signup" onClick={()=>{ way==='login'?setWay('register'):setWay('login')}}>{way==='login'?"立即注册":"立即登录"}</a></p>
+                                <p className="text-center"> <a data-toggle="tab" onClick={() => { way === 'login' ? setWay('register') : setWay('login') }}>{way === 'login' ? "立即注册" : "立即登录"}</a></p>
                             </div>
                         </div>
                     </div>
--- src/views/profile/profile.tsx
+++ src/views/profile/profile.tsx
@@ -2,7 +2,7 @@
  * @Author: linjingcheng 1152691418@qq.com
  * @Date: 2022-10-01 13:24:01
  * @LastEditors: linjingcheng 1152691418@qq.com
- * @LastEditTime: 2022-10-02 19:29:38
+ * @LastEditTime: 2022-10-03 10:25:33
  * @FilePath: \metabubble-join\src\views\profile\me.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -77,15 +77,13 @@ function Profile() {
         try {
             const res: any = await api.profile()
             setProfile(res)
-            if (res.schoolId === '') {
-                // console.log(res,'res',profile,'profile')
+            if (res.schoolId == '') {
                 history('/login')
             }
-            if (res.status !== -1) {
+            if (res.status != -1) {
                 history('/status')
             }
         } catch (error) {
-            history('/login')
             console.log(error, 'error')
         }
 
@@ -297,8 +295,7 @@ function Profile() {
                             item.url && (
                                 <div
                                     style={{
-                                        position: 'absolute',
-                                        bottom: 0,
+                                        position: 'relative',
                                         width: ' 100%',
                                         height: '100%',
                                         color: '#fff',
